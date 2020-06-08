9f66857e28e2ea379f0ba3fe61decbf5220a0009
snekcode <code.snakes@gmail.com>
2020-06-08 09:57:19 -1000

    added comments for better description of test file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -11,16 +11,33 @@ configure({testIdAttribute: 'id'})
 test('Select tester', async () => {
   const {getByTestId, queryByText, getByRole} = render(<App />);
 
+  // Find and save the select component as a varible
   const selectComp = getByTestId('select');
 
+  // We expect that the select component is in the document
   expect(selectComp).toBeInTheDocument()
+  // We expect it to have a default value
   expect(selectComp).toHaveTextContent("Testing1")
 
+  // And we can expect that 'Testing2' is not in the document important for the next expect statement
+  expect(queryByText('Testing2')).not.toBeInTheDocument()
+
+  // open the menu
   selectEvent.openMenu(selectComp);
+  // And we can expect that 'Testing2' is in that menu
   expect(queryByText('Testing2')).toBeInTheDocument()
-  await selectEvent.select(selectComp,/Testing2/);
+
+  // This line should select the value we want
+  // It is clear from this testing file that this is either not implemented correctly I'm using the latest version on NPM
+  await selectEvent.select(selectComp,"Testing2");
+
+  // for pretty console logs
   console.log(prettyDOM());
 
+  //This line should pass with an expected text content of "Testing2"
+  expect(selectComp).toHaveTextContent("Testing2")
+
+  // This should pass with an expected value of 2 which is the id of the label we selected
   expect(getByRole("form")).toHaveFormValues({test: "Testing2"});
 
 });
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Select from "react-select";
 
 function App() {
 
-    const optionArray = [{value: 1, label: "Testing1"},{value: 2, label: "Testing2"},{value: 3, label: "Testing3"}]
+    const optionArray = [{value: "Testing1", label: "Testing1"},{value: "Testing2", label: "Testing2"},{value: "Testing3", label: "Testing3"}]
     let customStyles: { singleValue: (base: any, {data}: { data: any }) => any; control: (base: any) => any; menu: (base:any) => any};
     customStyles = {
         singleValue: (base:any, {data}) => ({...base, color: "blue"}),
